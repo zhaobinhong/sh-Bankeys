4daed0653646ce967894de7292c49567ed5e222e
zhaobinhong <zhaobinhong29@163.com>
2017-09-11 16:47:22 +0800

    home

---
+++ src/components/home.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="main">
+    <div class="wrapper" style="overflow: auto">
+      <div class="li col-md-10 col-md-offset-1">
+        <div class="col-md-6 text-left">
+          <h4>高安全的身份电子标识</h4>
+          <p>由“公安部公民网络身份识别系统”签发给公民的网络电子身份标识，能够在不泄露身份信息的前提下在线远程识别身份。</p>
+        </div>
+        <div class="col-md-6 text-right"><img src="static/images/03.png" alt="高安全身份电子标识"></div>
+      </div>
+      <div class="li col-md-10 col-md-offset-1">
+
+        <div class="col-md-6 text-left"><img src="static/images/03.png" alt="高安全身份电子标识" ></div>
+        <div class="col-md-6 text-right">
+          <h4>高安全的身份电子标识</h4>
+          <p>由“公安部公民网络身份识别系统”签发给公民的网络电子身份标识，能够在不泄露身份信息的前提下在线远程识别身份。</p>
+        </div>
+      </div>
+      <div class="li col-md-10 col-md-offset-1">
+        <div class="col-md-6 text-left">
+          <h4>高安全的身份电子标识</h4>
+          <p>由“公安部公民网络身份识别系统”签发给公民的网络电子身份标识，能够在不泄露身份信息的前提下在线远程识别身份。</p>
+        </div>
+        <div class="col-md-6 text-right"><img src="static/images/03.png" alt="高安全身份电子标识"  ></div>
+      </div>
+      <div class="li end col-md-10 col-md-offset-1">
+
+        <div class="col-md-6 text-left"><img src="static/images/03.png" alt="高安全身份电子标识" ></div>
+        <div class="col-md-6 text-right">
+          <h4>高安全的身份电子标识</h4>
+          <p>由“公安部公民网络身份识别系统”签发给公民的网络电子身份标识，能够在不泄露身份信息的前提下在线远程识别身份。</p>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'main',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  div.li{
+    border-bottom: 1px solid #8c8c8c;
+  }
+
+  .li img{
+    display: inline-block;
+    display: -webkit-inline-box;
+  }
+
+  div.end{
+    border-bottom: none;
+  }
+</style>
